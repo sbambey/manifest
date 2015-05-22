57f76c9bc4556125f62ce73bac47a72a986bf589
Simon Bambey <sbambey.lp@gmail.com>
2015-05-22 13:22:39 -0700

    Minor layout adjustments on missions#index

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,9 +1,9 @@
 class MissionsController < ApplicationController
 	def index
 		Time.zone = 'Eastern Time (US & Canada)'
-		launch = Time.zone.parse('2015-06-26 11:09:00')
+		@launch = Time.zone.parse('2015-06-26 11:09:00')
 
-		diff = TimeDifference.between(launch, Time.now).in_each_component
+		diff = TimeDifference.between(@launch, Time.now).in_each_component
 
 		@day_diff = diff[:days].to_i
 		@hour_diff = (diff[:hours]-diff[:days].to_i*24).to_i
