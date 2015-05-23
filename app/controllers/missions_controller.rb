ce85d25d8265e7dc2f94e776ca07ecef8da2c2a7
Simon Bambey <sbambey.lp@gmail.com>
2015-05-22 17:59:10 -0700

    Remove auth restriction for mission index and show view

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,5 +1,5 @@
 class MissionsController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@missions = Mission.published.upcoming
