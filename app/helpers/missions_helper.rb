65faa2e8368d5978e9ce23faa648806804eb4a80
Simon Bambey <sbambey.lp@gmail.com>
2015-05-25 20:59:28 -0700

    Updates to personal implementation of time_diff

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -1,28 +1,33 @@
 module MissionsHelper
 	def time_diff(date, time)
 		str = ""
-		time_now = Time.now
+		time_now = Time.zone.now
+		date_today = Time.current.to_date
 		if !date.nil?
-			day_diff = TimeDifference.between(date, time_now).in_days.to_i
+			day_diff = (date-date_today).to_i
 			if day_diff > 0
-				str << "#{day_diff} days "
+				str << "#{day_diff} #{day_diff > 1 ? 'days' : 'day'}"
 			end
 		end
 
 		if !time.nil?
-			diff = TimeDifference.between(time, time_now).in_each_component
-			day_diff = diff[:days].to_i
-			hour_diff = (diff[:hours]-diff[:days].to_i*24).to_i
-			minute_diff = (diff[:minutes]-diff[:hours].to_i*60).to_i
-			second_diff = (diff[:seconds]-diff[:minutes].to_i*60).to_i
+			diff = time - time_now
+			day_diff = (diff/86400).to_i
+			hour_diff = ((diff-day_diff*86400)/3600).to_i
+			minute_diff = ((diff-hour_diff*3600-day_diff*86400)/60).to_i
+			second_diff = (diff-day_diff*86400-hour_diff*3600-minute_diff*60).to_i
 			if str.blank?
 				if hour_diff > 0
-					str << "#{hour_diff} hours, #{minute_diff} minutes, #{second_diff} seconds"
+					str << "#{hour_diff} #{hour_diff > 1 ? 'hours' : 'hour'}, 
+					#{minute_diff} #{minute_diff > 1 ? 'minutes' : 'minute'}, 
+					#{second_diff} #{second_diff > 1 ? 'seconds' : 'second'}"
 				else
-					str << "#{minute_diff} minutes, #{second_diff} seconds"
+					str << "#{minute_diff} #{minute_diff > 1 ? 'minutes' : 'minute' }, 
+					#{second_diff} #{second_diff > 1 ? 'seconds' : 'second'}"
 				end
 			else
-				str << "#{hour_diff} hours, #{minute_diff} minutes"
+				str << ", #{hour_diff} #{hour_diff > 1 ? 'hours' : 'hour'}, 
+				#{minute_diff} #{minute_diff > 1 ? 'minutes' : 'minute' }"
 			end
 		end
 		return str
